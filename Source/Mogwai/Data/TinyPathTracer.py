7b29738c22a3500d5c95a332db557477bfbe62e1
udemegane <cappuccino1204.ck@gmail.com>
2023-01-13 03:06:23 +0900

    Change renderpath link

--- Source/Mogwai/Data/TinyPathTracer.py
+++ Source/Mogwai/Data/TinyPathTracer.py
@@ -1,26 +1,26 @@
 from falcor import *
 
-def render_graph_CustomPathTracer():
-    g = RenderGraph("CustomPathTracer")
+def render_graph_TinyPathTracer():
+    g = RenderGraph("TinyPathTracer")
     loadRenderPassLibrary("AccumulatePass.dll")
     loadRenderPassLibrary("GBuffer.dll")
-    loadRenderPassLibrary("CustomPathTracer.dll")
+    loadRenderPassLibrary("TinyPathTracer.dll")
     loadRenderPassLibrary("ToneMapper.dll")
     AccumulatePass = createPass("AccumulatePass", {'enabled': True, 'precisionMode': AccumulatePrecision.Single})
     g.addPass(AccumulatePass, "AccumulatePass")
     ToneMapper = createPass("ToneMapper", {'autoExposure': False, 'exposureCompensation': 0.0})
     g.addPass(ToneMapper, "ToneMapper")
-    CustomPathTracer = createPass("CustomPathTracer", {'maxBounces': 3})
-    g.addPass(CustomPathTracer, "CustomPathTracer")
+    TinyPathTracer = createPass("TinyPathTracer", {'maxBounces': 3})
+    g.addPass(TinyPathTracer, "TinyPathTracer")
     VBufferRT = createPass("VBufferRT", {'samplePattern': SamplePattern.Stratified, 'sampleCount': 16})
     g.addPass(VBufferRT, "VBufferRT")
     g.addEdge("AccumulatePass.output", "ToneMapper.src")
-    g.addEdge("VBufferRT.vbuffer", "CustomPathTracer.vbuffer")
-    g.addEdge("VBufferRT.viewW", "CustomPathTracer.viewW")
-    g.addEdge("CustomPathTracer.color", "AccumulatePass.input")
+    g.addEdge("VBufferRT.vbuffer", "TinyPathTracer.vbuffer")
+    g.addEdge("VBufferRT.viewW", "TinyPathTracer.viewW")
+    g.addEdge("TinyPathTracer.color", "AccumulatePass.input")
     g.markOutput("ToneMapper.dst")
     return g
 
-CustomPathTracer = render_graph_CustomPathTracer()
-try: m.addGraph(CustomPathTracer)
+TinyPathTracer = render_graph_TinyPathTracer()
+try: m.addGraph(TinyPathTracer)
 except NameError: None
